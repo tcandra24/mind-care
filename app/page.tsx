2075ccc236efa99bb0b923e1afffe985609a3671
tcandra24 <tcandra007@gmail.com>
2025-11-01 10:35:30 +0700

    Rev. Update Under Maintance Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,7 @@
-// import LogoutButton from "@/components/logout-button";
-
-// export default function Home() {
-//   return (
-//     <main>
-//       <div>
-//         <h1>Home page</h1>
-//         <LogoutButton />
-//       </div>
-//     </main>
-//   );
-// }
-
-export default function Page() {
+export default function Home() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-      <p>Welcome</p>
+      <h1>Under Maintenance</h1>
     </div>
   );
 }
