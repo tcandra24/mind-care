e70c92ef97d920399ac0cc9d1d012485cc84b8d5
tcandra24 <tcandra007@gmail.com>
2025-11-01 13:32:18 +0700

    Ref Add Chart

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,13 @@
 import SectionCard from "@/components/section-card";
 import AlertWelcome from "@/components/alert-welcome";
+import ChartArea from "@/components/chart-area";
 
 export default function Dashboard() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
       <AlertWelcome />
       <SectionCard />
+      <ChartArea />
     </div>
   );
 }
