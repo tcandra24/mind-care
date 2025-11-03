6f3ea535d7cd181b8ee91469bdc50d11202586cf
Tito Candra <34393188+tcandra24@users.noreply.github.com>
2025-11-03 18:06:12 +0700

    Disabled theme provider

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -3,5 +3,6 @@
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  // return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return <>{children}</>;
 }
