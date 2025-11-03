9e8ccc400ec49fda0e2a1942ce9f361f56b69d3e
Tito Candra <34393188+tcandra24@users.noreply.github.com>
2025-11-03 17:56:16 +0700

    Update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
