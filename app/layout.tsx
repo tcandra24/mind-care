964450ba6cf0e57b8699bd4e3066850b78cd2959
tcandra24 <tcandra007@gmail.com>
2025-11-04 07:52:26 +0700

    Disabled All Feature Theme Switch

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,10 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          {children}
-          <Toaster position="top-center" />
-        </ThemeProvider>
+        {/* <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange> */}
+        {children}
+        <Toaster position="top-center" />
+        {/* </ThemeProvider> */}
       </body>
     </html>
   );
